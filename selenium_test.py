9d9fa40e6cd69e88baf0bb8ccc56061e6728b5ba
SethBergen <sbergen@ualberta.ca>
2018-11-02 17:15:14 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -5,4 +5,14 @@ def test_home():
 	driver = webdriver.Chrome()
 	driver.get("http://127.0.0.1:8000")
 	elem = driver.find_element_by_id("name")
-	assert elem != None 
+	assert elem != None
+	elem = driver.find_element_by_id("about")
+	assert elem != None
+	elem = driver.find_element_by_id("skills")
+	assert elem != None
+	elem = driver.find_element_by_id("education")
+	assert elem != None
+	elem = driver.find_element_by_id("work")
+	assert elem != None
+	elem = driver.find_element_by_id("contact")
+	assert elem != None
